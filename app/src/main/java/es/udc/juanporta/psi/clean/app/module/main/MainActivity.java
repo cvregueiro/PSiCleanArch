7be3ab23c49f41d7536f851a7af04a1ed9093c5b
Juan Porta Trinidad <juan.porta@udc.es>
2019-10-03 20:55:10 +0200

    Modelo Artista

--- app/src/main/java/es/udc/juanporta/psi/clean/app/module/main/MainActivity.java
+++ app/src/main/java/es/udc/juanporta/psi/clean/app/module/main/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import es.udc.juanporta.psi.clean.R;
+import es.udc.juanporta.psi.clean.app.domain.Artist;
 import es.udc.juanporta.psi.clean.app.module.BaseActivity;
 import es.udc.juanporta.psi.clean.app.module.main.adapter.ArtistAdapter;
 
@@ -36,16 +37,16 @@ public class MainActivity extends BaseActivity {
         mRecycler.setAdapter(adapter);
     }
 
-    private List<String> getArtists() {
+    private List<Artist> getArtists() {
 
-        List<String> artists = new ArrayList<>();
+        List<Artist> artists = new ArrayList<>();
 
-        artists.add("Ben Harper");
-        artists.add("Bon Iver");
-        artists.add("The National");
-        artists.add("Rancid");
-        artists.add("The Black Crows");
-        artists.add("The Band");
+        artists.add(new Artist(1, "Ben Harper"));
+        artists.add(new Artist(2, "Bon Iver"));
+        artists.add(new Artist(3, "The National"));
+        artists.add(new Artist(4, "Rancid"));
+        artists.add(new Artist(5, "The Black Crows"));
+        artists.add(new Artist(6, "The Band"));
 
         return artists;
     }
